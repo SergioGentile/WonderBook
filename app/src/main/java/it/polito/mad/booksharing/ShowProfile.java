099ba07ba4aeb4ba647bcc8cd9a55c5477bb5798
SergioGentile <sergio.gentile94@gmail.com>
2018-03-16 16:40:35 +0100

    First commit

---
+++ app/src/main/java/it/polito/mad/booksharing/ShowProfile.java
@@ -0,0 +1,15 @@
+package it.polito.mad.booksharing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_profile);
+
+
+    }
+}
