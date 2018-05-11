2ea283a32cf2c9c913230bcab710256c10f33425
SergioGentile <sergio.gentile94@gmail.com>
2018-05-11 19:44:42 +0200

    Completata la parte relativa alla cancellazione dei messaggi.

--- app/src/main/java/it/polito/mad/booksharing/NotificationService.java
+++ app/src/main/java/it/polito/mad/booksharing/NotificationService.java
@@ -38,7 +38,7 @@ public class NotificationService extends FirebaseMessagingService {
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
+       /* if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
             if(remoteMessage.getData().size() > 0){
                 HashMap<String,String> metaData = new HashMap<>(remoteMessage.getData());
@@ -66,7 +66,7 @@ public class NotificationService extends FirebaseMessagingService {
                 });
 
             }
-        }
+        }*/
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
@@ -80,7 +80,7 @@ public class NotificationService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void displayNotification(String messageBody,User sender,User receiver, String keyChat) {
-        Intent intent = new Intent(this, ChatPage.class);
+       /* Intent intent = new Intent(this, ChatPage.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         Bundle bundle = new Bundle();
@@ -91,7 +91,7 @@ public class NotificationService extends FirebaseMessagingService {
 
         String title = sender.getName().getValue() + " " + sender.getSurname().getValue();
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code *//*, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
         String channelId = "channel_chat";
@@ -117,11 +117,11 @@ public class NotificationService extends FirebaseMessagingService {
         }
 
         if(notificationManager != null){
-            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+            notificationManager.notify(0 /* ID of notification *//*, notificationBuilder.build());
         }
         else{
             Log.d("NOTIFICATION_MANAGER","Notification Manager Null Pointer");
-        }
+        }*/
     }
 
     protected void getUserInfoFromSharedPref() {
